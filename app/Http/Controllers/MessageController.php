fc30529d5467911614e4bf054ca1dfe294b9d236
njuma <nahshonjumaz@gmail.com>
2024-08-30 17:13:19 +0300

    max char

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -17,13 +17,13 @@ class MessageController extends Controller
             'email' => 'nullable|email|max:45',
             'phone' => 'nullable|string|regex:/^(\+?\d{1,4}[\s-]?)?(\(?\d{1,3}\)?[\s-]?)?[\d\s-]{5,20}$/|max:20', // Correct, allows phone numbers with optional country code, area code, and extension
             'subject' => 'required|max:25',
-            'message' => 'required|max:255',
+            'message' => 'required|max:1000',
             'latitude' => 'nullable|numeric',
             'longitude' => 'nullable|numeric',
             'ip' => 'nullable|ip',
             'city' => 'nullable|string|max:15',
             'region' => 'nullable|string|max:15',
-            'country' => 'nullable|string|max:4', 
+            'country' => 'nullable|string|max:4',
             'org' => 'nullable|string|max:255',
             'timezone' => 'nullable|string|max:50',
         ]);
