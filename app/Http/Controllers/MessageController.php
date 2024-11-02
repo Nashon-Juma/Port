1cac66dd81950a32000a4a65897948599dd11013
njuma <Nahshonjumaz@gmail.com>
2024-11-02 22:45:19 +0300

    update MessageController.php and master.css

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -15,7 +15,7 @@ class MessageController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:60',
             'email' => 'nullable|email|max:60',
-            'phone' => 'nullable',
+            'phone' => 'nullable|max:20',
             'subject' => 'required|max:225',
             'message' => 'required|max:2000',
 
